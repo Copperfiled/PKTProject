413baae7e94320e42f3121d288729b08a5401fd6
gityoung <gityoung@163.com>
2015-05-30 22:04:57 +0800

    Initial Commit

---
+++ PKTProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PKTProject
+//
+//  Created by young on 15/5/30.
+//  Copyright (c) 2015年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
